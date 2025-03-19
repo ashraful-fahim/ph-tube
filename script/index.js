d376386a9da630dd0d1fbfbf3e2987d3b683e4d7
ashraful-fahim <ashraful30june@gmail.com>
2025-03-19 17:27:04 +0600

    added the promised and fetched data dynamically to the page

--- script/index.js
+++ script/index.js
@@ -1,11 +1,28 @@
 function loadCategory() {
+    //1 - fetching the data
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then((res)=> res.json())
-    .then((data)=> displayCategories(data.categories));
+        //2 - convert promise to json
+        .then((res) => res.json())
+        //3 - send data to display
+        .then((data) => displayCategories(data.categories));
 }
 
 function displayCategories(categories) {
-    console.log(categories);
+    // get the container
+    const categoryContainer = document.getElementById('category-container');
+    // loop operation on array of object
+    for (let cat of categories) {
+        // create element
+        console.log(cat.category)
+        const categoryDiv = document.createElement('div');
+        categoryDiv.innerHTML = `
+        <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        `      
+        
+        // append element
+        categoryContainer.appendChild(categoryDiv);
+    }
+
 }
 
 loadCategory();
