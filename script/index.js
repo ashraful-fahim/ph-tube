ff6a11d4847c1723f07649c5fb1c55ede76b67e6
ashraful-fahim <ashraful30june@gmail.com>
2025-03-19 16:34:57 +0600

    connected to an API and made a promise

---
+++ script/index.js
@@ -0,0 +1,11 @@
+function loadCategory() {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then((res)=> res.json())
+    .then((data)=> displayCategories(data.categories));
+}
+
+function displayCategories(categories) {
+    console.log(categories);
+}
+
+loadCategory();
