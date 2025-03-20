339b2e0d27173202c0fe3c756720ef6f15ba076a
ashraful-fahim <ashraful30june@gmail.com>
2025-03-21 03:04:05 +0600

    fetched video data and added their dynamic details to all of them in the page

--- script/index.js
+++ script/index.js
@@ -38,49 +38,50 @@ const displayVideos = (videos) => {
     videos.forEach((video) => {
         const videoCard = document.createElement("div");
         videoCard.innerHTML = `
-        <div class="card bg-base-100 shadow-sm">
-  <figure>
-    <img
-      src="${video.thumbnail}"
-      alt="Shoes" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">${video.title}</h2>
-    <p>${video.description}</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+        <div class="card bg-base-100">
+            <figure class="relative">
+                <img class="w-full h-[180px] object-cover" src="${video.thumbnail}" alt="Shoes" />
+                <span class="absolute bottom-2 right-3 bg-black text-white rounded-sm p-1">3hrs 56 min ago</span>
+            </figure>
+            <div class="flex gap-4 py-3 pr-1 space-y-2">
+                <div>
+                    <div class="avatar">
+                        <div class="w-10 rounded-full">
+                            <img src="${video.authors[0].profile_picture}" />
+                        </div>
+                    </div>
+                </div>
+                <div class="intro">
+                    <h1 class="text-lg font-semibold">${video.title}</h1>
+                    <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name} <img class="w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""></p>
+                    <p class="text-sm text-gray-400">${video.others.views}</p>
+                </div>
+            </div>
+        </div>
         `
         videoContainer.appendChild(videoCard);
     });
 
 };
 
-
-
-// authors
-// : 
-// [{…}]
-// category_id
-// : 
-// "1001"
-// description
-// : 
-// "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// others
-// : 
-// {views: '100K', posted_date: '16278'}
-// thumbnail
-// : 
-// "https://i.ibb.co/L1b6xSq/shape.jpg"
-// title
-// : 
-// "Shape of You"
-// video_id
-// : 
-// "aaaa"
-
 loadCategory();
 loadVideo();
+
+    // {
+    // "category_id": "1001",
+    // "video_id": "aaaa",
+    // "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    // "title": "Shape of You",
+    // "authors": [
+    // {
+    // "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+    // "profile_name": "Olivia Mitchell",
+    // "verified": ""
+    // }
+    // ],
+    // "others": {
+    // "views": "100K",
+    // "posted_date": "16278"
+    // },
+    // "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+    // }
